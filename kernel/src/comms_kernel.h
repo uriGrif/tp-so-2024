aad86d4865b55266ca8b96f1122ba82dd35252ee
nicolau <marcosnicolau2003@gmail.com>
2024-04-21 15:33:03 -0300

    kernel io interface request

--- kernel/src/comms_kernel.h
+++ kernel/src/comms_kernel.h
@@ -3,6 +3,13 @@
 
 #include <proto/proto.h>
 #include <sockets/sockets.h>
+#include <interface.h>
+
+struct req_io_gen_sleep
+{
+    char *interface_name;
+    uint32_t work_units;
+};
 
 void handle_connections(void *void_args);
 
