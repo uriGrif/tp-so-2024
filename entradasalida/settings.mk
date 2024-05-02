b1dfec9e9a76ec1fb6644a5a66862b042a0d694f
nicolau <marcosnicolau2003@gmail.com>
2024-05-02 20:35:43 +0000

    config files for testing stdin/stdout

--- entradasalida/settings.mk
+++ entradasalida/settings.mk
@@ -9,7 +9,7 @@ CDEBUG=-g -Wall -DDEBUG -fdiagnostics-color=always
 CRELEASE=-O3 -Wall -DNDEBUG -fcommon
 
 # Arguments when executing with start, memcheck or helgrind
-ARGS=Int1 IO.config
+ARGS=Int2 IO_STDIN.config
 
 # Valgrind flags
 MEMCHECK_FLAGS=--track-origins=yes
