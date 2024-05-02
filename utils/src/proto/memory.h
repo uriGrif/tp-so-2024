b18b5f4bd9b2ab6e475e0eeca81e41cdf788b28f
nicolau <marcosnicolau2003@gmail.com>
2024-05-01 21:07:15 -0300

    memory: read_mem/write_mem protocols

---
+++ utils/src/proto/memory.h
@@ -0,0 +1,34 @@
+#include <proto/proto.h>
+
+typedef struct
+{
+    uint32_t pid;
+    char *address;
+    uint32_t offset;
+    uint32_t size;
+} t_memory_read_msg;
+
+typedef struct
+{
+    void *value;
+} t_memory_read_ok_msg;
+
+typedef struct
+{
+    int pid;
+    char *address;
+    uint32_t offset;
+    void *value;
+    uint32_t size;
+} t_memory_write_msg;
+
+int memory_send_read(int fd, int pid, char *address, uint32_t offset, uint32_t size);
+void memory_decode_read(t_buffer *buffer, t_memory_read_msg *msg);
+void memory_destroy_read(t_memory_write_msg *msg);
+
+int memory_send_read_ok(int fd, void *value, uint32_t size);
+void memory_decode_read_ok(t_buffer *buffer, t_memory_read_ok_msg *msg, uint32_t size);
+void memory_destroy_write(t_memory_write_msg *msg);
+
+int memory_send_write(int fd, int pid, char *address, uint32_t offset, void *value, uint32_t size);
+void memory_decode_write(t_buffer *buffer, t_memory_write_msg *msg);
