b7f4fa81f378194d1440cae813cbc18e885d0cfa
uriGrif <urielgrif@gmail.com>
2024-05-31 17:50:57 -0300

    calculo de direcciones a acceder

--- cpu/src/mmu.c
+++ cpu/src/mmu.c
@@ -9,7 +9,7 @@ uint32_t get_frame(int fd_mem, uint32_t pid, uint32_t page_number, t_log *logger
         {
             log_error(logger, "error al leer de la memoria");
             packet_free(packet);
-            return;
+            return 0;
         }
         frame_number = memory_decode_frame_number(packet->buffer);
         packet_free(packet);
@@ -37,4 +37,41 @@ uint32_t translate_address_1_byte(int fd_mem, uint32_t pid, uint8_t virtual_addr
 
     uint32_t physical_address = frame_number * page_size + offset;
     return physical_address;
+}
+
+t_access_to_memory* access_to_memory_create(uint32_t logical_address, uint32_t total_bytes, uint32_t page_size) {
+    t_access_to_memory *result = malloc(sizeof(t_access_to_memory));
+
+    uint32_t first_page = floor(logical_address / page_size);
+
+    uint32_t extra_page = logical_address % page_size == 0 ? 0 : 1;
+    result->pages_amount = total_bytes / page_size + extra_page;
+    result->addresses = malloc(sizeof(uint32_t) * result->pages_amount);
+    result->bytes_to_access = malloc(sizeof(uint32_t) * result->pages_amount);
+
+    int i = 0;
+
+    while (total_bytes > 0)
+    {
+        uint32_t offset = logical_address - (first_page + i) * page_size;
+        if (total_bytes >= page_size)
+        {
+            result->bytes_to_access[i] = page_size - offset;
+        }
+        else
+        {
+            result->bytes_to_access[i] = total_bytes;
+        }
+        total_bytes -= result->bytes_to_access[i];
+        logical_address += result->bytes_to_access[i];
+        i++;
+    }
+    
+    return result;
+}
+
+void access_to_memory_free(t_access_to_memory *a) {
+    free(a->addresses);
+    free(a->bytes_to_access);
+    free(a);
 }
--- cpu/src/mmu.h
+++ cpu/src/mmu.h
@@ -9,9 +9,20 @@
 #include <sys/socket.h>
 #include <proto/proto.h>
 #include <commons/log.h>
+#include <proto/memory.h>
+
+typedef struct {
+    uint32_t pages_amount; // cantidad de paginas a las que sera necesario acceder
+    uint32_t *addresses; // array del tamanio de la cantidad de paginas (cada elemento es una direccion en cada pagina) (son direcciones logicas, vamos agarrando una por una y las traducimos a fisicas)
+    uint32_t *bytes_to_access; // array (cada elemento son la cantidad de bytes a acceder desde cada direccion
+} t_access_to_memory;
 
 uint32_t translate_address_4_bytes(int fd_mem, uint32_t pid, uint32_t virtual_address, uint32_t page_size, t_log *logger);
 
 uint32_t translate_address_1_byte(int fd_mem, uint32_t pid, uint8_t virtual_address, uint32_t page_size, t_log *logger);
+
+t_access_to_memory* access_to_memory_create(uint32_t logical_address, uint32_t total_bytes, uint32_t page_size);
+
+void access_to_memory_free(t_access_to_memory *a);
 
 #endif
